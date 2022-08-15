32683b94e78e8653038d8844d3833e6a6583c4dc
António Azevedo <antonio.v.azevedo@gmail.com>
2022-08-15 18:03:58 +0100

    ft_parsing_utils function skeletons

--- srcs/parsing/ft_parsing_utils.c
+++ srcs/parsing/ft_parsing_utils.c
@@ -3,16 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   ft_parsing_utils.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lbraz-te <lbraz-te@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aazevedo <aazevedo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 13:49:11 by lbraz-te          #+#    #+#             */
-/*   Updated: 2022/08/15 13:50:59 by lbraz-te         ###   ########.fr       */
+/*   Updated: 2022/08/15 18:02:10 by aazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/miniRT.h"
 
-int		ft_parse_int(char	*str);
-float	ft_parse_float(char	*str);
-int		ft_parse_3int(char	*str, int pos);
-float	ft_parse_3float(char	*str, int pos);
+int	ft_parse_int(char	*str)
+{
+	(void)str;
+	return (0);
+}
+
+float	ft_parse_float(char	*str)
+{
+	(void) str;
+	return (0);
+}
+
+int	ft_parse_3int(char	*str, int pos)
+{
+	printf("ft_parse_3int()\n");
+	(void) str;
+	(void)pos;
+	return (0);
+}
+
+float	ft_parse_3float(char	*str, int pos)
+{
+	printf("ft_parse_3float()\n");
+	(void)str;
+	(void)pos;
+	return (0);
+}
